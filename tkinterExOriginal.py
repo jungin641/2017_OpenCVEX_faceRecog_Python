7b6131ca4363dea0119562f7b0ed697593d746ef
JunginYu <jungin641@gmail.com>
2017-03-13 16:06:59 +0900

    tkinterExOriginal.py : training btn 연결 완료

--- tkinterExOriginal.py
+++ tkinterExOriginal.py
@@ -1,7 +1,9 @@
+import os
 import numpy as np
 import cv2
 import Tkinter as tk
 import Image, ImageTk
+from PIL import Image
 
 #Set up GUI
 window = tk.Tk()  #Makes main window
@@ -21,10 +23,12 @@ cap = cv2.VideoCapture(0)
 faceDetect=cv2.CascadeClassifier('haarcascade_frontalface_default.xml');
 rec=cv2.createLBPHFaceRecognizer();
 rec.load("recognizer\\trainningData.yml")
-id=6
+id=5
 font=cv2.cv.InitFont(cv2.cv.CV_FONT_HERSHEY_COMPLEX_SMALL,5,1,0,4)
+#for training
+path='dataSet'
 
-def show_frame():
+def detect():
     _, frame = cap.read()
 
     frame = cv2.flip(frame, 1)
@@ -47,14 +51,68 @@ def show_frame():
     imgtk = ImageTk.PhotoImage(image=img)
     lmain.imgtk = imgtk
     lmain.configure(image=imgtk)
-    lmain.after(10, show_frame)
+    lmain.after(10, detect)
+
+def dataSetCreate():
+    print("dataSetCreateBtn pressed")
+    cap.release()
+    id = raw_input('enter user id')
+    sampleNum = 0;
+    while (True):
+        _, frameforDSC = cap.read();
+        frameforDSC = cv2.flip(frameforDSC, 1)
+        grayforDSC = cv2.cvtColor(frameforDSC, cv2.COLOR_BGR2GRAY)
+        faces = faceDetect.detectMultiScale(grayforDSC, 1.3, 5);
+        for (x, y, w, h) in faces:
+            sampleNum = sampleNum + 1;
+            cv2.imwrite("dataSet/User." + str(id) + "." + str(sampleNum) + ".jpg", grayforDSC[y:y + h, x:x + w])
+            cv2.rectangle(frameforDSC, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            cv2.waitKey(100);
+        if (sampleNum > 20):
+            break
+
+
+def getIamgesWithID(path):
+    imagePaths = [os.path.join(path, f) for f in os.listdir(path)]
+    faces = []
+    IDs = []
+    for imagePath in imagePaths:
+        faceImg = Image.open(imagePath).convert('L');
+        faceNp = np.array(faceImg, 'uint8')
+        ID = int(os.path.split(imagePath)[-1].split('.')[1])
+        faces.append(faceNp)
+        print ID
+        IDs.append(ID)
+        cv2.imshow("training", faceNp)
+        cv2.waitKey(10)
+    return np.array(IDs), faces
 
 
 
-#Slider window (slider controls stage position)
-sliderFrame = tk.Frame(window, width=600, height=100)
-sliderFrame.grid(row = 600, column=0, padx=10, pady=2)
+def training():
+    Ids, faces = getIamgesWithID(path)
+    rec.train(faces, Ids)
+    rec.save('recognizer/trainningData.yml')
+    cv2.destroyAllWindows()
 
 
-show_frame()  #Display 2
+#btn window (slider controls stage position)
+btnFrame = tk.Frame(window, width=600, height=100)
+btnFrame.grid(row = 600, column=0, padx=10, pady=2)
+
+dataSetCreateBtn = tk.Button(btnFrame,
+                    text="dataSetCreate", padx=10, pady=2, width=15,
+                    command=dataSetCreate)
+dataSetCreateBtn.pack(side=tk.LEFT)
+trainingBtn = tk.Button(btnFrame,
+                    text="Training", padx=10, pady=2, width=15,
+                    command=training)
+trainingBtn.pack(side=tk.LEFT)
+detectBtn = tk.Button(btnFrame,
+                    text="Detect", padx=10, pady=2, width=15,
+                    command=detect)
+detectBtn.pack(side=tk.LEFT)
+
+
+detect()  #Detect faces
 window.mainloop()  #Starts GUI
